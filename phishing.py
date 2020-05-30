8aa9e0f7f3caa638c0c1273b5fa4d283d6cfcd3a
aradhyagnov <66177011+aradhyagnov@users.noreply.github.com>
2020-05-30 23:17:35 +0530

    Add files via upload

---
+++ phishing.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 20:39:27 2020
+
+@author: Aradhya Goel
+"""
+
+#DATA ANALYSIS
+#1. Analyze the data.
+#2. We will load the phishing.csv file into the dataframe.
+#3. Then we will study the relation between the parametres and the result label.
+#4. We will then find out which parameters are significant and impact the results.
+#5. We will also analyze the nature of relationship (linear, exponential) between parameters and the result variable.
+#6. We will also analyze the data quality such as data completeness and consistency.
+
+#DATA PREPARATION
+#1. We will clean the data parameters to remove null values and empty figures.
+#2. We will ensure that the result column is not empty and has the correct labels.
+#3. we will also identify outliers and cardinality of the set.
+#4. we will also visualize relationship between feautures. We will do normalization of the data and binning.
+
+#MODELLING AND EVALUATION
+#1. We will identify the machine learnig regression model that can be applied to the data.
+#2. We will evaluate the model for its efficiency and we will use it to predict the future data.
+
+#DEPLOYMENT
+#1. We will deploy this model over cloud as a service.
